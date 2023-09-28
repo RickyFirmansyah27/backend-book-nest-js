3c34dfbec81ddc084f3cb525f95e84fd54b6c40c
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-09-28 15:59:45 +0700

    fixing e2e jest testing integration

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,14 +5,21 @@ import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let consoleLogSpy: jest.SpyInstance;
 
   beforeEach(async () => {
+    consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {}); // Memantau pemanggilan console.log
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterEach(() => {
+    consoleLogSpy.mockRestore(); // Mengembalikan fungsi console.log ke keadaan semula setelah pengujian
   });
 
   afterAll(async () => {
@@ -23,12 +30,29 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/users')
       .expect(200)
+      .expect((response) => {
+        expect(response.body.code).toBe('200');
+        expect(response.body.desc).toBe('success');
+        expect(response.body.data).toBeDefined();
+        expect(Array.isArray(response.body.data)).toBe(true);
+        // Jika Anda ingin menguji data pengguna tertentu dalam respons
+        expect(response.body.data[0][1].name).toBe('Ricky Firmansyah');
+        // Tambahkan pengujian lain sesuai kebutuhan
+      });
   });
-  
+
   it('/books (GET)', () => {
     return request(app.getHttpServer())
       .get('/books')
       .expect(200)
+      .expect((response) => {
+        expect(response.body.code).toBe('200');
+        expect(response.body.desc).toBe('success');
+        expect(response.body.data).toBeDefined();
+        expect(Array.isArray(response.body.data)).toBe(true);
+        // Jika Anda ingin menguji data pengguna tertentu dalam respons
+        expect(response.body.data[0][0].author).toBe('Ricky Firmansyah');
+        // Tambahkan pengujian lain sesuai kebutuhan
+      });
   });
-  
 });
