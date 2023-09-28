9bdfe5b42a0449bd6b15e64d7bd7b76096e8ed97
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-09-28 16:25:44 +0700

    add test with param and without param

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import * as querystring from 'querystring'; 
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -26,7 +27,7 @@ describe('AppController (e2e)', () => {
     await app.close(); // Menutup aplikasi setelah pengujian selesai
   });
 
-  it('/users (GET)', () => {
+  it('/users (GET) without query', () => {
     return request(app.getHttpServer())
       .get('/users')
       .expect(200)
@@ -41,7 +42,7 @@ describe('AppController (e2e)', () => {
       });
   });
 
-  it('/books (GET)', () => {
+  it('/books (GET) without query', () => {
     return request(app.getHttpServer())
       .get('/books')
       .expect(200)
@@ -50,9 +51,53 @@ describe('AppController (e2e)', () => {
         expect(response.body.desc).toBe('success');
         expect(response.body.data).toBeDefined();
         expect(Array.isArray(response.body.data)).toBe(true);
-        // Jika Anda ingin menguji data pengguna tertentu dalam respons
         expect(response.body.data[0][0].author).toBe('Ricky Firmansyah');
         // Tambahkan pengujian lain sesuai kebutuhan
       });
+  });
+
+  // Test dengan query params untuk /users
+  it('/users (GET) with query params', async () => {
+    const query = {
+      onYear: '20,23',
+      name: 'Ricky',
+      email: 'Ricky,Aeth',
+      filter: 'eyJhZ2UiOiAyM30=',
+    };
+
+    // Menggunakan querystring untuk memformat parameter query string
+    const queryStringFormatted = querystring.stringify(query);
+
+    const response = await request(app.getHttpServer())
+      .get(`/users?${queryStringFormatted}`)
+      .expect(200);
+
+    expect(response.body.code).toBe('200');
+    expect(response.body.desc).toBe('success');
+    expect(response.body.data).toBeDefined();
+    expect(Array.isArray(response.body.data)).toBe(true);
+    // Tambahkan pengujian lain sesuai kebutuhan
+  });
+
+  // Test dengan query params untuk /books
+  it('/books (GET) with query params', async () => {
+    const query = {
+      onYear: '2019,2023',
+      title: 'Myth of,Ragnarok',
+      author: 'Ricky,Aeth',
+    };
+
+    // Menggunakan querystring untuk memformat parameter query string
+    const queryStringFormatted = querystring.stringify(query);
+
+    const response = await request(app.getHttpServer())
+      .get(`/books?${queryStringFormatted}`)
+      .expect(200);
+
+    expect(response.body.code).toBe('200');
+    expect(response.body.desc).toBe('success');
+    expect(response.body.data).toBeDefined();
+    expect(Array.isArray(response.body.data)).toBe(true);
+    // Tambahkan pengujian lain sesuai kebutuhan
   });
 });
