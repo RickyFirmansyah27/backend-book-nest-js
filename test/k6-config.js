1a3585e0c233efb118262823ad35ffa9473cca9c
Ricky Firmansyah <ricky.firmansyah@dansmultipro.com>
2023-10-05 11:47:35 +0700

    fixing load test k6 using docker

--- test/k6-config.js
+++ test/k6-config.js
@@ -1,15 +1,58 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
-export const options = {
-  stages: [
-    { duration: '30s', target: 50 }, // Simulate 50 users for 30 seconds
-    { duration: '1m', target: 100 }, // Simulate 100 users for 1 minute
-    { duration: '30s', target: 0 }, // Ramp down to 0 users
-  ],
+export let options = {
+  vus: 1, // Jumlah pengguna virtual (Virtual Users) yang akan mengakses API
+  duration: '30s', // Durasi pengujian
 };
 
 export default function () {
-  const res = http.get('http://host.docker.internal:8000/books');
-  check(res, { 'status was 200': (r) => r.status == 200 });
+  // Pengujian /users (GET) tanpa query
+  let resUser = http.get('http://host.docker.internal:8000/users'); // Ganti URL sesuai dengan endpoint Anda
+  check(resUser, {
+    'status is 200': (r) => r.status === 200,
+    // Tambahkan pengujian lain sesuai kebutuhan
+  });
+  sleep(1);
+
+  // Pengujian /books (GET) tanpa query
+  let resBook = http.get('http://host.docker.internal:8000/books'); // Ganti URL sesuai dengan endpoint Anda
+  check(resBook, {
+    'status is 200': (r) => r.status === 200,
+    // Tambahkan pengujian lain sesuai kebutuhan
+  });
+  sleep(1);
+
+  // Pengujian /users (GET) dengan query params
+  const query = {
+    onYear: '20,23',
+    name: 'Ricky',
+    email: 'Ricky,Aeth',
+    filter: 'eyJhZ2UiOiAyM30=',
+  };
+  const queryStringFormatted = Object.keys(query)
+    .map((key) => `${key}=${query[key]}`)
+    .join('&');
+  let resQueryUser = http.get(`http://host.docker.internal:8000/users?${queryStringFormatted}`);
+  check(resQueryUser, {
+    'status is 200': (r) => r.status === 200,
+    // Tambahkan pengujian lain sesuai kebutuhan
+  });
+  sleep(1);
+
+  // Pengujian /books (GET) dengan query params
+  const bookQuery = {
+    onYear: '2019,2023',
+    title: 'Myth of,Ragnarok',
+    author: 'Ricky,Aeth',
+  };
+  const bookQueryStringFormatted = Object.keys(bookQuery)
+    .map((key) => `${key}=${bookQuery[key]}`)
+    .join('&');
+  let resQueryBook = http.get(`http://host.docker.internal:8000/books?${bookQueryStringFormatted}`);
+  check(resQueryBook, {
+    'status is 200': (r) => r.status === 200,
+    // Tambahkan pengujian lain sesuai kebutuhan
+  });
+  sleep(1);
 }
